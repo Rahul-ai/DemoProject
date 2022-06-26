f92eeec698ff2146f9e830d4a68e4149713c34e3
Rahul-ai <rt878777@gmail.com>
2022-06-26 15:25:22 +0530

    .Updated Attendence

--- src/Controller/AttendanceController.php
+++ src/Controller/AttendanceController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Attendence;
+use App\Form\GetAttendence;
+use App\Form\AttendenceType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AttendanceController extends AbstractController
 {
-    #[Route('/attendance', name: 'app_attendance')]
-    public function index(): Response
+    #[Route('/attendance', name: 'attendance')]
+    public function index(Request $request): Response
     {
-        return $this->render('attendance/index.html.twig', [
-            'controller_name' => 'AttendanceController',
-        ]);
+        $Attendence = new Attendence();
+        $form = $this->createForm(GetAttendence::class,$Attendence);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $newClass = $form->getData(); 
+            $Date = new Date();
+            $id = 1;
+            
+        $RAW_QUERY = 'SELECT a.Admission_Number,  FROM student a
+        LEFT JOIN classes b ON a.class_id = $id  
+        LEFT JOIN attendence c ON a.id = c.student_id AND c.Date = $Date
+        GROUP BY a.id';
+
+        $Attendance = $this->repo->RawQuery($RAW_QUERY);
+    
+        return $this->render('attendance/index.html.twig', [   
+            'form' => $form->createView(),   
+            'Addendance' => $Attendance
+        ]);  
+        }    
+        return $this->render('attendance/index.html.twig', [   
+            'form' => $form->createView(),
+        ]);  
     }
 }
---
+++ src/Form/GetAttendence.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classes;
+use App\Entity\Student;
+use App\Entity\Attendence;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class GetAttendence extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ClassId',EntityType::class,[
+             'class' => Classes::class,
+             'attr' => array()
+            ])
+            ->add('Date',DateType::class,[
+               'attr' => array()
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Attendence::class,
+        ]);
+    }
+}
